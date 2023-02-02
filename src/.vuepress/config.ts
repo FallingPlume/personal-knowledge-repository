a9a8411fe52a4fe55138cc14177957c2af43ac71
FallingPlume <merous@126.com>
2023-02-02 16:55:39 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  
   locales: {
     "/": {
       lang: "zh-CN",
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -4,4 +4,13 @@ export const zhSidebar = sidebar({
   "/": [
     ""
   ],
+  "/java/": [
+    "",
+    {
+      text: "Java 基础",
+      prefix: "/java/javabase/",
+      collapsible: true,
+      children: ["基础语法.md"]
+    }
+  ]
 });
